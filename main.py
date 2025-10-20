039db04b636f991c110bafc32bbfe4d8721fea1c
spekkie300 <speklapje300@gmail.com>
2025-10-20 15:46:09 +0200

    Implemented Character counting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_word_count
+from stats import get_char_count
 
 
 def get_book_text(filepath):
@@ -11,8 +12,10 @@ def main():
     fp = "books/frankenstein.txt"
     book_string = get_book_text(fp)
     wc = get_word_count(book_string)
+    char_count = get_char_count(book_string)
 
     print(f"Found {wc} total words")
+    print(char_count)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,13 @@
 def get_word_count(string):
     return len(string.split())
+
+
+def get_char_count(string):
+    lowered_string = string.lower()
+    char_dict = {}
+    for char in lowered_string:
+        if char not in char_dict:
+            char_dict[char] = 1
+        else:
+            char_dict[char] += 1
+    return char_dict
